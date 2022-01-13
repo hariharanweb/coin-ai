3ad7f06ada1058888595d88fafbd2dd08ea15485
HARIHARAN THIAGARAJAN <hariharanweb@gmail.com>
2022-01-13 10:09:03 +0530

    Handle errors in candle fetching

--- source/services/candleService.js
+++ source/services/candleService.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 import stub from './stub';
-import _ from 'lodash';
+import _, { isError } from 'lodash';
 
 const fetchCandles = async (marketPair, limit = 180) => {
-    const response = await axios.get(`https://public.coindcx.com/market_data/candles?pair=${marketPair}&interval=1m&limit=${limit}`);
+    const response = await axios
+        .get(`https://public.coindcx.com/market_data/candles?pair=${marketPair}&interval=1m&limit=${limit}`)
+        .catch(error => {
+            console.log(error.response.data);
+            return [];
+        });
     // const response = stub;
     return response.data.map((candle, index) => {
         return {
